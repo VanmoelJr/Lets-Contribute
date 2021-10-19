5612739ae874528a5b214a03c2fa1ac8fa105f6a
Muhammad Maulana <vanmoeljr8@gmail.com>
2021-10-19 19:31:52 +0700

    Added JavaScript File

---
+++ JavaScript/data-table.js
@@ -0,0 +1,28 @@
+//[Data Table Javascript]
+
+//Project:  Fox Admin - Responsive Admin Template
+
+//Primary use:   Used only for the Data Table
+
+$(function () {
+    "use strict";
+
+    $('#example1').DataTable();
+    $('#example2').DataTable({
+      'paging'      : true,
+      'lengthChange': false,
+      'searching'   : false,
+      'ordering'    : true,
+      'info'        : true,
+      'autoWidth'   : false
+    });
+	
+	
+	$('#example').DataTable( {
+		dom: 'Bfrtip',
+		buttons: [
+			'copy', 'csv', 'excel', 'pdf', 'print'
+		]
+	} );
+	
+  }); // End of use strict
